f7f4e44c828092eadd3b09096345608735a3bf9b
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-04-10 11:36:02 -0400

    added connection to database with mongoose

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const connectDB = require('./config/database')
 const PORT = 8000
+
+connectDB()
 
 app.get('/', (request, response) => {
   response.sendFile(__dirname + '/test.html')
